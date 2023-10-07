037eeeef8c970d4d3e42dc5a7686e5f86b9ce7e7
Juan Carlos Hernandez <jchernandez0312@gmail.com>
2023-10-06 23:32:05 -0500

    Created project

---
+++ SaveLink/SaveLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaveLinkApp.swift
+//  SaveLink
+//
+//  Created by Juan Carlos Hernandez Castillo on 6/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SaveLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
